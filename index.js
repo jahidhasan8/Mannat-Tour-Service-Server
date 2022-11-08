96a9eb541514312297b25a27744478ca74952001
jahidhasan8 <jahidhasan1932@gmail.com>
2022-11-09 02:51:37 +0600

    update and delete and get api created for reviews part

--- index.js
+++ index.js
@@ -36,12 +36,61 @@ async function run(){
             res.send(service)
          });
 
+         app.get('/reviews',async(req,res)=>{
+     
+             let query={}
+             if(req.query.email){
+                 query={
+                     email:req.query.email
+                 }
+             }
+             else if(req.query.serviceId){
+                query={
+                    serviceId:req.query.serviceId
+                }
+             }
+             const cursor=reviewCollection.find(query)
+             const reviews=await cursor.toArray()
+             res.send(reviews)
+         })
+        
+         app.get('/reviews/:id',async(req,res)=>{
+            const id=req.params.id
+            const query={_id:ObjectId(id)}
+            const review=await reviewCollection.findOne(query);
+            res.send(review)
+        })
+
          app.post('/reviews',async(req,res)=>{
             const review=req.body 
             const result=await reviewCollection.insertOne(review);
             res.send(result)
     
         });
+
+        app.put('/reviews/:id',async(req,res)=>{
+            const id=req.params.id
+            const filter={_id:ObjectId(id)}
+            const review=req.body
+            const option={upsert:true};
+            const updatedReview={
+                $set:{
+                    customer:review.customer,
+                    text:review.text,
+                    rating:review.rating,
+                    photo:review.photo
+                }
+            }
+          const result=await reviewCollection.updateOne(filter,updatedReview,option);
+            res.send(result);
+        })
+
+        app.delete('/reviews/:id',async(req,res)=>{
+            const id=req.params.id
+            const query={_id:ObjectId(id)}
+            const result=await reviewCollection.deleteOne(query)
+            res.send(result)
+        })
     }
     finally{
 
